322a588e15c20f5b3c3f66910cb7186e12cdcf24
Samuel Arandia <samuelarandia@gmail.com>
2024-07-02 13:35:55 -0400

    som changes for SEO

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,7 +1,7 @@
 import ogImageSrc from "@images/tool2.jpeg";
 
 export const SITE = {
-  title: "A toda makina",
+  title: "Arriendo de Herramientas y Maquinaria | A Toda Makina",
   tagline: "Arriendo de maquinaria, herramientas y equipos para la construcción",
   description: "A toda makina ofrece el servicio de arriendo de maquinaria, herramientas y equipos para la construcción como placas compactadoras, martillos demoledores, rodillos, escaleras, generadores eléctricos, entre otros. Contactanos para más información.",
   description_short: "A toda makina ofrece el servicio de arriendo de maquinaria, herramientas y equipos para la construcción.",
@@ -33,7 +33,7 @@ export const OG = {
   locale: "es_CL",
   type: "website",
   url: SITE.url,
-  title: `${SITE.title}: : Herramientas y equipos para la construcción`,
+  title: `${SITE.title}: : Arriendo de Herramientas y equipos para la construcción`,
   description: "En A toda makina ofrecemos herramientas, maquinas, grupos electrogenos y equipos para la construcción, arriendo de maquinaria, herramientas y equipos para la construcción.",
   image: ogImageSrc,
 };
